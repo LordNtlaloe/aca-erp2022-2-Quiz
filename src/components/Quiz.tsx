97302cca1d7401582573921bb3a0274cb4fd1f11
LordNtlaloe <ntlal0e182@gmail.com>
2024-07-04 22:12:44 +0200

    Adjusted Questions and Resolved Issue Causing Vercel Error

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -4,6 +4,7 @@ import { Question1to10 } from '../data/Question1to10';
 import { Question11to20 } from '../data/Question11to20';
 import { Question21to30 } from '../data/Question21to30';
 import { Question31to40 } from '../data/Question31to40';
+import { Question41to50 } from '../data/Question41to50';
 
 const Quiz = () => {
     const [currentQuestion, setCurrentQuestion] = useState<number>(0);
@@ -27,7 +28,8 @@ const Quiz = () => {
     ...Question1to10,
     ...Question11to20,
     ...Question21to30,
-    ...Question31to40
+    ...Question31to40,
+    ...Question41to50
   ]
   return (
     <div className="max-w-2xl mx-auto p-4">
