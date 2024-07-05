87858700eaaff0134f8ba76250b38f9aa844ba05
LordNtlaloe <ntlal0e182@gmail.com>
2024-07-05 13:23:49 +0200

    Added Vercel Token

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -10,6 +10,13 @@ const Quiz = () => {
     const [currentQuestion, setCurrentQuestion] = useState<number>(0);
     const [score, setScore] = useState<number>(0);
     const [showScore, setShowScore] = useState<boolean>(false);
+    const QuizQuestions = [
+      ...Question1to10,
+      ...Question11to20,
+      ...Question21to30,
+      ...Question31to40,
+      ...Question41to50
+    ]
 
   const handleAnswerOptionClick = (isCorrect:boolean) => {
     if (isCorrect) {
@@ -17,20 +24,14 @@ const Quiz = () => {
     }
 
     const nextQuestion = currentQuestion + 1;
-    if (nextQuestion < Question1to10.length) {
+    if (nextQuestion < QuizQuestions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
       setShowScore(true);
     }
   };
 
-  const QuizQuestions = [
-    ...Question1to10,
-    ...Question11to20,
-    ...Question21to30,
-    ...Question31to40,
-    ...Question41to50
-  ]
+
   return (
     <div className="max-w-2xl mx-auto p-4">
       {showScore ? (
